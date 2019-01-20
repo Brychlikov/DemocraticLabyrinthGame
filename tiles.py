06ac9bc69d1ccd53e017f12a74064e1810130e34
Brychlikov <brychlikow@gmail.com>
2019-01-20 23:42:29 +0100

    Some more documentation

--- tiles.py
+++ tiles.py
@@ -7,13 +7,19 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self, settings, x, y):
         super().__init__()
 
+        # global settings object
         self.settings: game.Settings = settings
+
+        # image of the tile. Black square by default
         self.image = pygame.Surface((settings.tile_size, settings.tile_size))
+
+        # rect representing sprite's position on the screen
         self.rect = self.image.get_rect()
 
         self._pos_x = None
         self._pos_y = None
 
+        # these represent tile's position in game coordinates
         self.pos_x = x
         self.pos_y = y
 
