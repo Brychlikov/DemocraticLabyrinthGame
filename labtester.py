7dc63bd4ec4da4b99aa8b611b7d0c9dd80f13eac
Brychlikov <brychlikow@gmail.com>
2019-01-22 15:33:59 +0100

    add lab_tester

---
+++ labtester.py
@@ -0,0 +1,27 @@
+import pygame
+from new_temp import actually_gen_lab
+
+tile_size = 30
+lab_size = 12
+wall_list = actually_gen_lab(10)
+
+display = pygame.display.set_mode((tile_size * lab_size, tile_size * lab_size))
+pygame.display.set_caption("Wielki generator labiryntów wielkiego i groźniego bazeli")
+
+done = False
+clock = pygame.time.Clock()
+while not done:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            done = True
+
+    display.fill((0, 0, 0))
+    wall_list = actually_gen_lab(10)
+    for w in wall_list:
+        pygame.draw.line(display, (0, 0, 255), (w.x1 * tile_size, w.y1 * tile_size), (w.x2 * tile_size, w.y2 * tile_size), 3)
+
+    pygame.display.flip()
+
+    clock.tick(0.3)
+
+pygame.quit()
